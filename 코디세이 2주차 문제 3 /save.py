108a68727e54e4fe35e19dbc4ba11265be5e1644
geemongshell <145413231+geemongshell@users.noreply.github.com>
2025-03-20 15:41:58 +0900

    Update save.py

--- 코디세이 2주차 문제 3 /save.py
+++ 코디세이 2주차 문제 3 /save.py
@@ -1,5 +1,3 @@
-import csv
-
 input_file_path = 'C:\\Users\\dino1\\codyssey\\문제3\\Sorted_Flammability_List.csv'
 output_file_path = 'C:\\Users\\dino1\\codyssey\\문제3\\Mars_Base_Inventory_danger.csv'
 
@@ -7,10 +5,11 @@ dangerous_items = []
 
 # 정렬된 인화성 목록 파일 읽기
 with open(input_file_path, 'r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    header = next(reader)  # 첫 번째 행을 헤더로 저장
+    lines = file.readlines()
+    header = lines[0].strip().split(',')  # 첫 번째 행을 헤더로 저장
     
-    for row in reader:
+    for line in lines[1:]:
+        row = line.strip().split(',')
         try:
             if float(row[1]) >= 0.7:  # 인화성 지수가 0.7 이상인 경우
                 dangerous_items.append(row)
@@ -22,9 +21,9 @@ print('인화성 지수가 0.7 이상인 목록:')
 print([header] + dangerous_items)
 
 # 위험 목록을 새로운 CSV 파일로 저장
-with open(output_file_path, 'w', encoding='utf-8', newline='') as output_file:
-    writer = csv.writer(output_file)
-    writer.writerow(header)
-    writer.writerows(dangerous_items)
+with open(output_file_path, 'w', encoding='utf-8') as output_file:
+    output_file.write(','.join(header) + '\n')
+    for row in dangerous_items:
+        output_file.write(','.join(row) + '\n')
 
 print(f'위험 목록이 {output_file_path} 파일로 저장되었습니다.')
