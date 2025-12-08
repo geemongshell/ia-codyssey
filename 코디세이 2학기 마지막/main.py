b50d9e9fff2d6dc9771e46970e9a92c523d24897
geemongshell <145413231+geemongshell@users.noreply.github.com>
2025-12-08 17:20:16 +0900

    Initialize FastAPI application with database setup

---
+++ 코디세이 2학기 마지막/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+from database import Base, engine
+from domain.question.question_router import router as question_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(question_router)
