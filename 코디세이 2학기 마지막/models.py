3689ed8cbc591480a77f636f9118f273219ca755
geemongshell <145413231+geemongshell@users.noreply.github.com>
2025-12-08 17:19:20 +0900

    Add Question model to models.py

---
+++ 코디세이 2학기 마지막/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+
+from database import Base
+
+
+class Question(Base):
+    __tablename__ = 'question'
+
+    id = Column(Integer, primary_key=True, index=True)
+    subject = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    create_date = Column(DateTime, default=datetime.utcnow)
